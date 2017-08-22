c922159b6a8f7eed77de1fe38d26ff49988df27e
osblow <osblow.wang@gmail.com>
2017-08-22 11:16:10 +0800

    base functions
    base functions

---
+++ Assets/CustomScrollRect.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Oslobw.UI.Foundation
+{
+    public enum OrientaionType
+    {
+        Horizontal = 0,
+        Vertical = 1
+    }
+
+    [RequireComponent(typeof(Image))]
+    [RequireComponent(typeof(Mask))]
+    public class CustomScrollRect : MonoBehaviour
+    {
+        public OrientaionType Orientation;
+
+        [Space]
+        public Transform Content;
+        public float MoveSpeed = 800;
+
+
+
+        private GridLayoutGroup m_grid;
+        private QuickGrid m_quickGrid;
+
+        private Vector3 m_lastMousePos;
+        private Vector3 m_curMousePos;
+
+        private Vector3 m_contentMoveTarget = Vector3.zero;
+        private bool m_startMove = false;
+
+        private const float c_move_sensitive = 10;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            if (!Content)
+            {
+                Debug.LogError("No scroll content attached!!");
+                return;
+            }
+            
+            m_contentMoveTarget = Content.position;
+            m_grid = Content.GetComponent<GridLayoutGroup>();
+            m_quickGrid = new QuickGrid(GetComponent<RectTransform>(), m_grid);
+
+            if(m_grid) StartCoroutine(DisableGridLater());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!CheckValid())
+            {
+                return;
+            }
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_lastMousePos = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                m_curMousePos = Input.mousePosition;
+
+                Vector2 mouseSpeed = GetMoveSpeed();
+
+                if (Orientation == OrientaionType.Vertical)
+                {
+                    // vertical
+                    if (Mathf.Abs(mouseSpeed.y) > c_move_sensitive)
+                    {
+                        m_contentMoveTarget = Content.localPosition + Vector3.up * mouseSpeed.y;
+                        m_startMove = true;
+                        m_quickGrid.OnScroll(Orientation);
+                    }
+                }
+                else
+                {
+                    // horizontal
+                    if (Mathf.Abs(mouseSpeed.x) > c_move_sensitive)
+                    {
+                        m_contentMoveTarget = Content.localPosition + Vector3.right * mouseSpeed.x;
+                        m_startMove = true;
+                        m_quickGrid.OnScroll(Orientation);
+                    }
+                }
+
+                m_lastMousePos = m_curMousePos;
+            }
+
+            Move();
+        }
+
+        void Move()
+        {
+            if (!m_startMove)
+            {
+                return;
+            }
+
+            if(Vector3.Distance(Content.position, m_contentMoveTarget) < 1)
+            {
+                m_startMove = false;
+                return;
+            }
+
+            Content.localPosition = Vector3.MoveTowards(Content.localPosition, m_contentMoveTarget, Time.deltaTime * MoveSpeed);
+        }
+
+
+        Vector2 m_mouseSpeed = new Vector2();
+        Vector2 GetMoveSpeed()
+        {
+            m_mouseSpeed.x = m_curMousePos.x - m_lastMousePos.x;
+            m_mouseSpeed.y = m_curMousePos.y - m_lastMousePos.y;
+            if(m_mouseSpeed.x < 0.5f && m_mouseSpeed.x > -0.5f)
+            {
+                m_mouseSpeed.x = 0;
+            }
+            if(m_mouseSpeed.y < 0.5f && m_mouseSpeed.y > -0.5f)
+            {
+                m_mouseSpeed.y = 0;
+            }
+            
+            return m_mouseSpeed;
+        }
+
+
+        bool CheckValid()
+        {
+            if (!Content)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        IEnumerator DisableGridLater()
+        {
+            yield return new WaitForSeconds(0.3f);
+            m_grid.enabled = false;
+        }
+    }
+}
---
+++ Assets/QuickGrid.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Oslobw.UI.Foundation
+{
+    /// <summary>
+    /// 暂不支持动态grid
+    /// </summary>
+    public class QuickGrid
+    {
+        public QuickGrid(RectTransform scrollRect, GridLayoutGroup grid)
+        {
+            m_scrollRect = scrollRect;
+            m_grid = grid;
+        }
+
+
+        RectTransform m_scrollRect;
+        GridLayoutGroup m_grid;
+
+        
+        public void OnScroll(OrientaionType orientation)
+        {
+            if (!m_grid)
+            {
+                return;
+            }
+
+            int activeRangeMin;
+            int activeRangeMax;
+
+            if (orientation == OrientaionType.Vertical)
+            {
+                activeRangeMin = Mathf.FloorToInt((m_grid.transform.position.y - m_scrollRect.position.y - m_scrollRect.sizeDelta.y / 2) / m_grid.cellSize.y);
+                activeRangeMax = Mathf.FloorToInt((m_grid.transform.position.y - m_scrollRect.position.y + m_scrollRect.sizeDelta.y / 2) / m_grid.cellSize.y);
+            }
+            else
+            {
+                activeRangeMin = Mathf.FloorToInt((m_grid.transform.position.x - m_scrollRect.position.x - m_scrollRect.sizeDelta.x / 2) / m_grid.cellSize.x);
+                activeRangeMax = Mathf.FloorToInt((m_grid.transform.position.x - m_scrollRect.position.x + m_scrollRect.sizeDelta.x / 2) / m_grid.cellSize.x);
+            }
+            
+            for (int i = 0; i < m_grid.transform.childCount; i++)
+            {
+                SetActive(m_grid.transform.GetChild(i).gameObject, i >= activeRangeMin && i <= activeRangeMax);
+            }
+        }
+
+        void SetActive(GameObject g, bool active)
+        {
+            if(g.activeInHierarchy != active)
+            {
+                g.SetActive(active);
+            }
+        }
+    }
+}
